1f50b63dbd4f391165eccbcfae545c7819e5f439
Mahfuz Mia <95216952+mahfuz1703@users.noreply.github.com>
2022-02-21 21:43:26 +0600

    Add files via upload

---
+++ uri_1006.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    double MEDIA,A,B,C;
+	scanf("%lf",&A);
+	scanf("%lf",&B);
+	scanf("%lf",&C);
+	MEDIA=(A*2+B*3+C*5)/(2+3+5);
+	printf("MEDIA = %0.1lf\n",MEDIA);
+    return 0;
+}
