3fb6f3ed25bb940fa2dcdd8ecb0f988096931e90
Mahfuz Mia <95216952+mahfuz1703@users.noreply.github.com>
2022-02-21 21:44:31 +0600

    Add files via upload

---
+++ uri_1007.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int DIF,A,B,C,D;
+	scanf("%d",&A);
+	scanf("%d",&B);
+	scanf("%d",&C);
+	scanf("%d",&D);
+	DIF =(A*B)-(C*D);
+	printf("DIFERENCA = %d\n",DIF);
+    return 0;
+}
