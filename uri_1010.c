df8ca062b2fb0a0f14292a59c8a0831b469e2c2c
Mahfuz Mia <95216952+mahfuz1703@users.noreply.github.com>
2022-02-21 23:33:57 +0600

    Add files via upload

---
+++ uri_1010.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    double c, result;
+
+    scanf("%d %d %lf", &a, &b, &c);
+    result = b * c;
+
+    scanf("%d %d %lf", &a, &b, &c);
+    result += b * c;
+
+    printf("VALOR A PAGAR: R$ %.2lf\n", result);
+    return 0;
+}
